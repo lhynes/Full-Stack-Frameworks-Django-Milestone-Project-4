cd51c9cb7a70fa1aa8f114f7b7f2ddff6401aa60
lhynes <louise.m.hynes@gmail.com>
2021-03-17 21:21:36 +0000

    added header media and updated navbar

---
+++ static/js/style.js
@@ -0,0 +1,20 @@
+ // transparent navbar turns white when scrolling down
+// And returns transparent when is at the top of the screen
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+// for message time on screen
+// will delete image
+
+
+setTimeout(function() {
+  let delete_message = document.getElementById("message_container");
+  delete_message.style.display = "none";
+}, 3000);
