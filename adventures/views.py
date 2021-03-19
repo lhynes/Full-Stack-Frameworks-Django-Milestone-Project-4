3e61fbf0381457aca96b306dc502a1a83358ed0a
lhynes <louise.m.hynes@gmail.com>
2021-03-19 11:18:26 +0000

    added adventure view and template page

--- adventures/views.py
+++ adventures/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Adventure
+
 
 # Create your views here.
+
+def all_adventures(request):
+    """ A view to return the adventures listview page, includeing sorting and searching """
+
+    adventures = Adventure.objects.all()
+
+    context = {
+        'adventures': adventures,
+    }
+
+    return render(request, 'adventures/adventures.html',context)
