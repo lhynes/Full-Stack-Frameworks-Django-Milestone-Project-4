b9db9d32752b1d6a43797c124c3845fa38cb391c
lhynes <louise.m.hynes@gmail.com>
2021-03-19 13:45:14 +0000

    added adventure detail functionality

--- adventures/urls.py
+++ adventures/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_adventures, name='adventures')
+    path('', views.all_adventures, name='adventures'),
+    path('<adventure_id>', views.adventure_detail, name='adventure_detail')
 ]
--- adventures/views.py
+++ adventures/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Adventure
 
 
@@ -13,4 +13,15 @@ def all_adventures(request):
         'adventures': adventures,
     }
 
-    return render(request, 'adventures/adventures.html',context)
+    return render(request, 'adventures/adventures.html', context)
+
+def adventure_detail(request, adventure_id):
+    """ A view to return an individual adventure detail page """
+
+    adventure = get_object_or_404(Adventure, pk=adventure_id)
+
+    context = {
+        'adventure': adventure,
+    }
+
+    return render(request, 'adventures/adventure_detail.html', context)
