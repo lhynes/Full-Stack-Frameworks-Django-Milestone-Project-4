61e22f15ac21e1721146efa3b91e24bd511d0f62
lhynes <louise.m.hynes@gmail.com>
2021-03-20 16:31:57 +0000

    updated context processor and added cart nav icon css

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,16 +1,33 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from adventures.models import Adventure
+
 
 def bag_contents(request):
 
     bag_items = []
     total = 0
     adventure_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        adventure = get_object_or_404(Adventure, pk=item_id)
+        total += item_data * adventure.price
+        adventure_count += item_data
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'adventure': adventure,
+        })
+
+    grand_total = total
 
     context = {
         'bag_items': bag_items,
         'total': total,
         'adventure_count': adventure_count,
+        'grand_total': grand_total,
     }
 
     return context
