fdee5f8368f043375576309a596df5136e786b2d
lhynes <louise.m.hynes@gmail.com>
2021-03-21 12:05:25 +0000

    updated urls.py and added checkout template

--- em_travel/urls.py
+++ em_travel/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('adventures/', include('adventures.urls')),
     path('bag/', include('bag.urls')),
-
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
