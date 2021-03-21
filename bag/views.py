72a9b63830ffd46f883e5d65371cae752b3609a0
lhynes <louise.m.hynes@gmail.com>
2021-03-21 09:28:42 +0000

    added all toasts to files

--- bag/views.py
+++ bag/views.py
@@ -20,6 +20,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Added {adventure.name} to your bag')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {adventure.name} to your bag')
