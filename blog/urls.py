84cb497d6d7605011eebf8ba56a4525229d0877e
lhynes <louise.m.hynes@gmail.com>
2021-03-27 23:40:46 +0000

    added , to end of path

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_blog, name='view_blog')
+    path('', views.view_blog, name='view_blog'),
     # path('', views.all_posts, name='blog'),
     path('<slug:slug>/', views.blog_post_detail, name='blog_post_detail'),
 ]
