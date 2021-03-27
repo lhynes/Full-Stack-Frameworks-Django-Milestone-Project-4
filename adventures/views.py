9e0168d73cf66dd35fe24149968b12f9441ea8d3
lhynes <louise.m.hynes@gmail.com>
2021-03-27 21:18:50 +0000

    added security to views

--- adventures/views.py
+++ adventures/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q  # Used to generate a search query 
 from django.db.models.functions import Lower
 
@@ -39,6 +40,7 @@ def all_adventures(request):
 
     return render(request, 'adventures/adventures.html', context)
 
+
 def adventure_detail(request, adventure_id):
     """ A view to return an individual adventure detail page """
 
@@ -51,8 +53,13 @@ def adventure_detail(request, adventure_id):
     return render(request, 'adventures/adventure_detail.html', context)
 
 
+@login_required
 def add_adventure_package(request):
     """ Add an adventure to the site """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = AdventurePackageForm(request.POST, request.FILES)
         if form.is_valid():
@@ -63,7 +70,7 @@ def add_adventure_package(request):
             messages.error(request, 'Failed to add adventure package. Please ensure the form is valid.')
     else:
         form = AdventurePackageForm()
-    
+        
     template = 'adventures/add_adventure_package.html'
     context = {
         'form': form,
@@ -72,8 +79,13 @@ def add_adventure_package(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_adventure_package(request, adventure_id):
     """ Edit an adventure on the site """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     adventure = get_object_or_404(Adventure, pk=adventure_id)
     if request.method == 'POST':
         form = AdventurePackageForm(request.POST, request.FILES, instance=adventure)
@@ -96,8 +108,13 @@ def edit_adventure_package(request, adventure_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_adventure_package(request, adventure_id):
     """ Delete a adventure from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     adventure = get_object_or_404(Adventure, pk=adventure_id)
     adventure.delete()
     messages.success(request, 'Adventure package deleted!')
