a0e83749083c638e4e420cf6e282e71a41eb0d5c
lhynes <louise.m.hynes@gmail.com>
2021-03-29 16:08:35 +0000

    updated UI elements

--- home/views.py
+++ home/views.py
@@ -4,20 +4,22 @@ from adventures.models import Adventure
 
 # Create your views here.
 
+
 def index(request):
     """ A view to return the index page  and show adventures"""
-    
-    return render(request, 'home/index.html')
+    show_adventures = Adventure.objects.all()[:3] # limit to 3
+    context = {
+        'show_adventures': show_adventures,
+    }
+    return render(request, 'home/index.html', context)
 
 
 def show_adventures(request):
     """ A view to load the home page with adventures """
-    
     show_adventures = Adventure.objects.all()
-
     context = {
-       'show_adventures': show_adventures,
+        'show_adventures': show_adventures,
     }
-   
+
     # show_adventures is to loop adventures on the home page.
     return render(request, 'home/index.html', context)
