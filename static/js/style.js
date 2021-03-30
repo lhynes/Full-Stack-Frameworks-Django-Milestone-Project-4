b6853e6bdc3a036bf5cef39a175eb8b3e33cb546
lhynes <louise.m.hynes@gmail.com>
2021-03-30 16:57:44 +0000

    updated nav logos

--- static/js/style.js
+++ static/js/style.js
@@ -4,8 +4,12 @@ $(function () {
     $(window).on('scroll', function () {
         if ( $(window).scrollTop() > 10 ) {
             $('.navbar').addClass('active');
+            $('img.nav_logo').attr('src','/static/images/5.png');
         } else {
             $('.navbar').removeClass('active');
+            $('img.nav_logo').attr('src','/static/images/4.png');
         }
     });
 });
+
+
